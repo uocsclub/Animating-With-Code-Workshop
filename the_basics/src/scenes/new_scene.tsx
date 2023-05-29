66d5f19907954589c3e4e8db87d740694f3e4797
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2023-05-29 18:14:03 -0400

    Update new_scene.tsx

--- the_basics/src/scenes/new_scene.tsx
+++ the_basics/src/scenes/new_scene.tsx
@@ -1,35 +1,45 @@
 import { Circle, Layout, Rect, View2D } from '@motion-canvas/2d/lib/components';
 import {makeScene2D} from '@motion-canvas/2d/lib/scenes';
+import { waitFor } from '@motion-canvas/core/lib/flow';
+
+const size_unit = 100;
 
 function* scene_hierarchy_1(view: View2D) {
     //creating elements using variables
-    let circle = new Circle({width: 1, fill:"red"});
+    let circle = new Circle({size: size_unit * 2, fill:"red"});
 
     view.add([circle]);
 
-    view.add([new Circle({width: 1, fill:"green"})]);
+    view.add([new Circle({size: size_unit * 1, fill:"green"})]);
+
+    yield* waitFor(2)
 }
 function* scene_hierarchy_2(view: View2D) {
     //creating elements using jsx
     view.add(
         <>
-            <Circle width={2} fill={"red"}>
+            <Circle size={size_unit * 2} fill={"red"}>
             </Circle>
-            <Circle width={1} fill={"green"}>
+            <Circle size={size_unit * 1} fill={"green"}>
             </Circle>
         </>
     );
+    yield* waitFor(2)
 }
 function* scene_hierarchy_3(view: View2D) {
     //creating elements using variables
-    let circle = new Circle({width: 1, fill:"red"});
+    let circle = new Circle({size: size_unit * 2, fill:"red"});
 
     view.add([circle]);
 
-    view.add([new Circle({width: 1, fill:"green"})]);
+    view.add([new Circle({size: size_unit * 1, fill:"green"})]);
 
     // removing first circle
+    yield* waitFor(1);
+
     circle.remove();
+    
+    yield* waitFor(1);
 }
 
 function* positioning(view: View2D) {
@@ -49,4 +59,4 @@ function* simple_animation(view: View2D) {
 
 }
 
-export default makeScene2D(scene_hierarchy_1);
+export default makeScene2D(scene_hierarchy_3);
